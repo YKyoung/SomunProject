9ac14b8076a03cc6cb7b5f7baaba503ad8960a98
YKyoung <scc050905young@gmail.com>
2021-01-27 15:04:05 +0900

    main.css, main.js, index.html 변경

---
+++ main.js
@@ -0,0 +1,36 @@
+const cm = {
+    canvas: undefined,
+    context: undefined,
+    canvasWidth: 0,
+    canvasHeight: 0
+};
+
+(function(){
+
+    cm.canvas = document.querySelector('#the-canvas');
+    cm.context = cm.canvas.getContext('2d');
+    const canvasContainer = document.querySelector('.canvas-container');
+    const dpr = window.devicePixelRatio > 1 ? 2 : 1;
+
+    function setSize() {
+        cm.canvasWidth = canvasContainer.clientWidth;
+        cm.canvasHeight = canvasContainer.clientHeight;
+        cm.canvas.width = cm.canvasWidth;
+        cm.canvas.height = cm.canvasHeight;
+
+    } 
+
+    function setup() {
+        setSize();
+        draw();
+    }
+
+    function draw() {
+
+    }
+
+    window.addEventListener('resize', setSize);
+    window.addEventListener('load', setup);
+
+
+})();
